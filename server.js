db3df7ba71294dabea02611115c5843dfb41d2a8
Manu Arora <mannuarora700@gmail.com>
2018-06-30 15:49:42 +0530

    Users profiles and posts done

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
@@ -15,6 +18,11 @@ mongoose
 
 app.get('/', (req, res) => res.send('Hello World'));
 
+// use Routes
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
