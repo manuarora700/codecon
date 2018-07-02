d595b2ee29584d5e9ab01a9ae3db86a0407e04d3
Manu Arora <mannuarora700@gmail.com>
2018-07-02 17:56:42 +0530

    Bug fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // Load user model
 const User = require('../../models/User');
@@ -15,5 +17,32 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
 router.post('/register', (req, res) => {
 	User.findOne({ email: req.body.email })
+		.then(user => {
+			if(user) {
+				return res.status(400).json({email: 'Email already exists'});
+			} else {
+				const avatar = gravatar.url(req.body.email, {
+					s: '200',
+					r: 'pg',
+					d: 'mm'
+				});
+				const newUser = new User({
+					name: req.body.name,
+					email: req.body.email,
+					avatar,
+					password: req.body.password
+				});
+
+				bcrypt.genSalt(10, (err, salt) => {
+					bcrypt.hash(newUser.password, salt, (err, hash) => {
+						if(err) throw err;
+						newUser.password = hash;
+						newUser.save()
+							.then(user => res.json(user))
+							.catch(err => console.log(err));
+					})
+				})
+			}
+		})
 });
 module.exports = router;
