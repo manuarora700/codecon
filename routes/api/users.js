612518f6b6e89db314d588f2f5093443318b0b50
Manu Arora <mannuarora700@gmail.com>
2018-07-02 17:40:43 +0530

    Bug fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Load user model
+const User = require('../../models/User');
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+
+router.post('/register', (req, res) => {
+	User.findOne({ email: req.body.email })
+});
 module.exports = router;
