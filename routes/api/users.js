e6c3e7b8af33f70d9196c7486d158f22c8842c66
Manu Arora <mannuarora700@gmail.com>
2018-07-03 18:12:13 +0530

    JWT stuff

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,4 +45,31 @@ router.post('/register', (req, res) => {
 			}
 		})
 });
+
+// @route   GET api/users/login
+// @desc    Login user / returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// Find the user by email
+	User.findOne({email})
+		.then(user => {
+			// Check for user
+			if(!user) {
+				return res.status(400).json({email: 'User not found'});
+
+			}
+			// Check password
+			bcrypt.compare(password, user.password)
+				.then(isMatch => {
+					if(isMatch) {
+						res.json({msg: 'Success'});
+					} else {
+						return res.status(400).json({password: 'Password incorrect'});
+					}
+				})
+		})
+});
 module.exports = router;
