f7bb5ec9cfffd5dbde4108d36d94604d58f6120e
Manu Arora <mannuarora700@gmail.com>
2018-07-05 13:13:22 +0530

    Server side validation complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load input validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load user model
 const User = require('../../models/User');
@@ -63,6 +64,12 @@ router.post('/register', (req, res) => {
 // @desc    Login user / returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+	const { errors, isValid } = validateLoginInput(req.body);
+
+	// Check validation
+	if(!isValid) {
+		return res.status(400).json(errors);
+	}
 	const email = req.body.email;
 	const password = req.body.password;
 
@@ -71,7 +78,8 @@ router.post('/login', (req, res) => {
 		.then(user => {
 			// Check for user
 			if(!user) {
-				return res.status(400).json({email: 'User not found'});
+				errors.email = 'User not found';
+				return res.status(400).json(errors);
 
 			}
 			// Check password
@@ -94,7 +102,8 @@ router.post('/login', (req, res) => {
 								});
 							});
 					} else {
-						return res.status(400).json({password: 'Password incorrect'});
+						errors.password = 'Password incorrect';
+						return res.status(400).json(errors);
 					}
 				})
 		})
