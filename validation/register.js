a58517e608be9d0da3b7c501d22d726f4c40c2ce
Manu Arora <mannuarora700@gmail.com>
2018-07-05 12:22:12 +0530

    Tricky validation bruv

--- validation/register.js
+++ validation/register.js
@@ -4,10 +4,15 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
 	let errors = {};
 
+	data.name = !isEmpty(data.name) ? data.name : '';
+
 	if(!Validator.isLength(data.name, { min: 2, max: 30 })) {
 		errors.name = 'Name must be between 2 and 30 characters';
 	}
 
+	if(Validator.isEmpty(data.name)) {
+		errors.name = 'Name field is required';
+	}
 	return {
 		errors, 
 		isValid: isEmpty(errors)
