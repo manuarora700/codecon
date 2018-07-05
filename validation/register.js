ae6423866aca1d5503409c5be595c946053c0bec
Manu Arora <mannuarora700@gmail.com>
2018-07-05 12:04:39 +0530

    Custom function isEmpty created

--- validation/register.js
+++ validation/register.js
@@ -6,4 +6,9 @@ module.exports = function validateRegisterInput(data) {
 	if(!Validator.isLength(data.name, { min: 2, max: 30 })) {
 		errors.name = 'Name must be between 2 and 30 characters';
 	}
+
+	return {
+		errors, 
+		isValid: errors
+	}
 }
