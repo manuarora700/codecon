d1fe6a7031bdf8988e103601a08a577a846565ce
Manu Arora <mannuarora700@gmail.com>
2018-07-05 12:11:52 +0530

    Adding more validation

--- validation/register.js
+++ validation/register.js
@@ -1,4 +1,5 @@
 const Validator = require('validator');
+const isEmpty = require('./is-empty');
 
 module.exports = function validateRegisterInput(data) {
 	let errors = {};
@@ -9,6 +10,6 @@ module.exports = function validateRegisterInput(data) {
 
 	return {
 		errors, 
-		isValid: errors
+		isValid: isEmpty(errors)
 	}
 }
