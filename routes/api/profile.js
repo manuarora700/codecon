ab8b4b1cfff87473a0299893b58cda184f050220
Manu Arora <mannuarora700@gmail.com>
2018-07-07 11:10:32 +0530

    Working with private routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load profile model
+const Profile = require('../../models/Profile');
+// Load user model
+const User = require('../../models/User');
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+
+router.get('/', passport.authenticate('jwt', { session: false }),(req, res) => {
+
+});
+
+
 
 module.exports = router;
